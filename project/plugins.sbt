bb9e38424940b9c8ea8e5f4f28ca363684e522e6
Wudong Liu <wudong.liu@gmail.com>
2022-06-21 21:11:11 +0100

    Only change the publishing name when it is a plugin.
    - When it is not a plugin, there is no need to redefine the publishing;
    - Also changed the publishing pattern to use the static sbt cross version, to be consistent with the reading side;

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,7 +1,7 @@
 resolvers+= "Artifactory" at "https://wudong.jfrog.io/artifactory/default-maven-local"
 
 libraryDependencies += {
-  "io.github.esbeetee" % "sbt-consistent_2.12_1.0" % "0.4.0"
+  "io.github.esbeetee" % "sbt-consistent_2.12_1.0" % "0.4.4"
 }
 
 addSbtPlugin("com.github.sbt" % "sbt-release" % "1.1.0")
--- src/main/scala/sbtconsistent/SbtConsistentPlugin.scala
+++ src/main/scala/sbtconsistent/SbtConsistentPlugin.scala
@@ -6,10 +6,9 @@ import sbt.Keys.*
 object SbtConsistentPlugin extends AutoPlugin {
   // override def requires = sbt.plugins.SbtPlugin
   override def trigger = allRequirements
+  val SbtCrossName = "sbt1_2.12"
 
   object autoImport {
-    //// create the sbtCrossVersion for the current for plugin.
-    lazy val sbtCrossVersion = settingKey[String]("Scala and SBT cross version for plugin project")
     lazy val sbtCrossArtifactName = settingKey[String]("Scala and SBT crossed name for plugin project")
     lazy val makeProperPom = taskKey[File]("Make a proper POM for plugin project")
     lazy val moduleIdTransformer = settingKey[Seq[ModuleID=>ModuleID]]("List of transforms to add a plugin")
@@ -24,7 +23,7 @@ object SbtConsistentPlugin extends AutoPlugin {
         val scalaV = (update / scalaBinaryVersion).value
         val originalName = m.name
         val name = originalName.stripSuffix(s"_$sbtV").stripSuffix(s"_$scalaV")
-        m.withName(s"${name}_sbt1_2.12")
+        m.withName(s"${name}_${}")
           .withExtraAttributes(Map.empty)
       }
     ),
@@ -32,58 +31,59 @@ object SbtConsistentPlugin extends AutoPlugin {
 
   override lazy val projectSettings: Seq[Setting[?]] = Seq(
 
-    sbtCrossVersion := {
-      val scala = scalaBinaryVersion.value
-      val sbt = sbtBinaryVersion.value
-      s"${scala}_$sbt"
-    },
+      sbtCrossArtifactName := {
+        val artifactName = artifact.value.name
+        s"${artifactName}_sbt1_2.12"
+      },
 
-    sbtCrossArtifactName := {
-      val artifactName = artifact.value.name
-      val crossVersion = sbtCrossVersion.value
-      s"${artifactName}_$crossVersion"
-    },
+      makeProperPom := {
+        val originalPomFile = makePom.value
+        val fileNew = (makeProperPom / artifactPath).value
+        val artifactName = artifact.value.name
+        val crossedName = sbtCrossArtifactName.value
 
-    makeProperPom := {
-      val originalPomFile = makePom.value
-      val fileNew = (makeProperPom / artifactPath).value
-      val artifactName = artifact.value.name
-      val crossedName = sbtCrossArtifactName.value
+        IO.writeLines(
+          fileNew,
+          IO.readLines(originalPomFile)
+            .map(_.replace(s"<artifactId>$artifactName</artifactId>", s"<artifactId>$crossedName</artifactId>"))
+        )
 
-      IO.writeLines(
-        fileNew,
-        IO.readLines(originalPomFile)
-          .map(_.replace(s"<artifactId>$artifactName</artifactId>", s"<artifactId>$crossedName</artifactId>"))
-      )
+        streams.value.log.info(s"Created a proper POM file: $fileNew")
+        fileNew
+      },
 
-      streams.value.log.info(s"Created a proper POM file: $fileNew")
-      fileNew
-    },
+      makeProperPom / artifactPath := {
+        val originalPomFile = (makePom / artifactPath).value// crossVersion
+        val fileName =  sbtCrossArtifactName.value + ".pom"
+        originalPomFile.getParentFile / fileName
+      },
 
-    makeProperPom / artifactPath := {
-      val originalPomFile = (makePom / artifactPath).value// crossVersion
-      val fileName =  sbtCrossArtifactName.value + ".pom"
-      originalPomFile.getParentFile / fileName
-    },
+      makeProperPom / artifact := {
+        val name = (makePom / artifact).value.name
+        val newName = sbtCrossArtifactName.value
+        (makePom / artifact).value.withName(newName)
+      },
 
-    makeProperPom / artifact := {
-      val name = (makePom / artifact).value.name
-      val newName = sbtCrossArtifactName.value
-      (makePom / artifact).value.withName(newName)
-    },
+      // add the new pom to the list of artifact to be published.
+      artifacts += (makeProperPom / artifact).value,
 
-    // add the new pom to the list of artifact to be published.
-    artifacts += (makeProperPom / artifact).value,
+      publishM2 / packagedArtifacts := Def.taskDyn {
+         val isPlugin = sbtPlugin.value
+         if (isPlugin) {
+           Def.task {
+             Map(
+               (Compile / packageBin / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageBin).value,
+               (Compile / packageDoc / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageDoc).value,
+               (Compile / packageSrc / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageSrc).value,
+               (makeProperPom / artifact).value -> makeProperPom.value)
+           }
+         } else {
+           publishM2 / packagedArtifacts
+         }
+      }.value,
 
-    publishM2 / packagedArtifacts := Map(
-            ((Compile / packageBin / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageBin).value),
-            ((Compile / packageDoc / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageDoc).value),
-            ((Compile / packageSrc / artifact).value.withName(sbtCrossArtifactName.value) -> (Compile / packageSrc).value),
-            ((makeProperPom / artifact).value -> makeProperPom.value)),
-
-    publish / packagedArtifacts :=  (publishM2 / packagedArtifacts).value
-
-  )
+      publish / packagedArtifacts := (publishM2 / packagedArtifacts).value
+    )
 
   // an method help to resolve dependencies with consistent POM.
   def addConsistentSbtPlugin(dependency: ModuleID): Setting[Seq[ModuleID]] = {
